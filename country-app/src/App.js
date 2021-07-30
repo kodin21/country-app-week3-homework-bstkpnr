5accc4149c6f032619d4660d657492972b717120
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-07-30 18:21:55 +0300

    completed static list

--- country-app/src/App.js
+++ country-app/src/App.js
@@ -1,44 +1,85 @@
-import './App.css';
 import Country from './component/Country/Country';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import { css } from "@emotion/react";
+import './App.css'
 
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import {Nav, NavItem} from 'react-bootstrap'
+import { Button} from 'react-bootstrap'
 import LanguageStatic from './component/Language/Language';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './component/DarkMode/globalStyles';
+import {lightTheme,darkTheme} from './component/DarkMode/Themes';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react'
+import BounceLoader from "react-spinners/BounceLoader";
 
+const API_URL='https://restcountries.eu/rest/v2/all'
+const override = css`
+  display: block;
+  margin: 0 auto;
+  position: relative;
+  opacity: 2.5; 
+  border:4px;       
+  top: 0;
+  left: 0;
+  animation-fill-mode: both;
+            
 
-function App() {
-  return (
-    <Router>
-      <div className="container ">
-          <Nav variant="pills" defaultActiveKey="/">
-            <NavItem>
-              <Nav.Link href="/" >
-                Ülkeler Listesi
-              </Nav.Link>
-            </NavItem>
-            <NavItem>
-              <Nav.Link href="/static">
-                İstatistik
-
-              </Nav.Link>
-
-            </NavItem>
-          </Nav>
-            </div>
-
-        <Route path="/" component={Country} exact />
-        <Route path="/static" component={LanguageStatic} />
-    </Router>
-  );
-}
-const linkStyle = {
-  margin: "0 0.8rem",
-  textDecoration: "none",
-  color: 'blue',
-  border:'2px solid',
-  position: 'relative',
 
  
-  
-};
+`;
+function App() {
+  const [loading, setLoading]=useState(false);
+
+  const [theme, setTheme]=useState('light');
+  const themeToggler=()=>{
+    theme ==='light'? setTheme('dark'):setTheme('light')
+  }
+  const [countrySelected, setCountrySelected]=useState(true);
+  const[data,setData]=useState({countries:[]});
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+
+    },2000)
+    axios.get(API_URL).then((data) => {
+      setData({
+        countries:data
+      });
+      
+    });
+  }, []);
+  return (
+    <div className="App">
+
+      {
+        loading ?
+        <BounceLoader color={'#055052'} loading={loading}  size={250}  css={override}/>
+        
+        :
+        
+        <ThemeProvider theme={theme==='light'?lightTheme:darkTheme}>
+      <>
+      <GlobalStyles/>
+
+        <BootstrapSwitchButton onChange={themeToggler} size="sm" onstyle="dark" offstyle="light" style="border" className="py-3"></BootstrapSwitchButton>
+   
+   <div className=" mr-3 text-center py-2 mb-3">
+
+         <Button variant="info" className="mt-3 py-3 mx-2" onClick={()=>setCountrySelected(true)}>Ülke Listesi</Button>
+         <Button variant="light" className="mt-3 py-3 mx-2" onClick={()=>setCountrySelected(false)}>İstatistik Listesi</Button>
+       {countrySelected && (<Country />)} 
+         {!countrySelected && (<LanguageStatic />)} 
+   </div>
+
+
+    
+    </>
+    </ThemeProvider>
+  }
+    </div>
+  );
+  }
+
 export default App;
--- country-app/src/component/Country/Country.js
+++ country-app/src/component/Country/Country.js
@@ -1,38 +1,40 @@
 import React, { useState,useEffect } from "react";
 import axios from 'axios'
-import {Card,CardColumns,CardGroup,CardImg,Row,Col} from 'react-bootstrap'
+import {Card,CardImg,Row,Col} from 'react-bootstrap'
 
 
 
+const API_URL='https://restcountries.eu/rest/v2/all'
 function Country() {
+
   const [countries, setCountries] = useState([]);
   useEffect(() => {
-    axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
+    axios.get(API_URL).then((res) => {
       setCountries(res.data);
     });
   }, [countries]);
   return (
-  
-    <Row xs={1} md={3} className="g-4">
+    
+    <Row xs={1} md={4} className="g-4">
 
-    {countries.map((eleman) => {
+    {countries.map((element) => {
       return (
-        <Col>
-            <Card key={eleman.name} border="success" className="bg-white text-dark" style={{height:"50vh",border:"4px solid"}}>
-                <CardImg variant="top" src={eleman.flag}  />
+        <Col className="py-2">
+            <Card key={element.name} className="light shadow-lg p-3 mb-5 bg-gray rounded text-dark  " style={{height:"50vh",border:"3px solid"}}>
+                <CardImg variant="top" src={element.flag} style={{height:"25vh", width:"auto"}} />
                 <Card.Body>
-                  <Card.Title>{eleman.name}</Card.Title>
-                  <Card.Title className="mb-2 text-muted">
-                    {eleman.capital}
+                  <Card.Title>{element.name}</Card.Title>
+                  <Card.Title className="mb-3 text-muted">
+                    {element.capital}
                   </Card.Title>
-                  <Card.Text>{eleman.languages[0].name}</Card.Text>
+                  <Card.Text>{element.languages[0].name}</Card.Text>
                 </Card.Body>
                 </Card>
                 </Col>
           );
         })}
         </Row>
-    
+      
   );
 }
 
